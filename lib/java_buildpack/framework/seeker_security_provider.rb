2cf4e1e8e1f88503489dec386bc4d2c31e283d4e
Moshe Apelbaum <apelbaum@synopsys.com>
2018-06-07 16:42:13 +0300

    rename variable

--- lib/java_buildpack/framework/seeker_security_provider.rb
+++ lib/java_buildpack/framework/seeker_security_provider.rb
@@ -17,7 +17,6 @@
 
 require 'java_buildpack/component/base_component'
 require 'java_buildpack/framework'
-require 'uri'
 
 module JavaBuildpack
   module Framework
@@ -38,8 +37,7 @@ module JavaBuildpack
         download_zip('', URI.join(credentials[ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY],
                                   SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER), false, sensor_dir)
         shell "unzip -qq #{File.join(sensor_dir, 'SensorInstaller.zip')} -d #{@droplet.sandbox} 2>&1"
-        shell "unzip -qq #{File.join(sensor_dir, 'SeekerInstaller.jar')} #{AGENT_JARS_RELATIVE_PATH_IN_JAR} \\
-          -d #{sensor_dir} 2>&1"
+        shell "unzip -qq #{File.join(sensor_dir, 'SeekerInstaller.jar')} #{AGENT_JARS_PATH} -d #{sensor_dir} 2>&1"
         shell "rm -rf #{sensor_dir}"
         @droplet.copy_resources
       end
@@ -82,14 +80,14 @@ module JavaBuildpack
       SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER = 'rest/ui/installers/binaries/LINUX'
 
       # Relative path of the agent jars after Sensor extraction
-      AGENT_JARS_RELATIVE_PATH_IN_JAR = 'inline/agents/java/'
+      AGENT_JARS_PATH = 'inline/agents/java/'
 
       # seeker service name identifier
       FILTER = /seeker/
 
       private_constant :SEEKER_HOST_SERVICE_CONFIG_KEY, :SEEKER_HOST_PORT_SERVICE_CONFIG_KEY,
                        :ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY, :SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER,
-                       :AGENT_JARS_RELATIVE_PATH_IN_JAR
+                       :AGENT_JARS_PATH
     end
   end
 end
