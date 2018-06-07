51e879b30db0e1438d23af5fa7935e1c4f4d3cb9
Moshe Apelbaum <apelbaum@synopsys.com>
2018-06-07 15:31:31 +0300

    SKP-4295 fix lint errors

--- lib/java_buildpack/framework/seeker_security_provider.rb
+++ lib/java_buildpack/framework/seeker_security_provider.rb
@@ -34,11 +34,13 @@ module JavaBuildpack
       def compile
         credentials = fetch_credentials
         assert_configuration_valid(credentials)
-        sensor_dir = File.join(@droplet.sandbox,'seeker_tmp_sensor')
+        sensor_dir = File.join(@droplet.sandbox, 'seeker_tmp_sensor')
         shell "rm -rf #{sensor_dir}"
-        download_zip('', URI.join(credentials[ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY],SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER), false, sensor_dir)
-        shell "unzip -qq #{File.join(sensor_dir,'SensorInstaller.zip')} -d #{@droplet.sandbox} 2>&1"
-        shell "unzip -qq #{File.join(sensor_dir,'SeekerInstaller.jar')} #{AGENT_JARS_RELATIVE_PATH_IN_JAR} -d #{sensor_dir} 2>&1"
+        download_zip('', URI.join(credentials[ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY],
+                                  SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER), false, sensor_dir)
+        shell "unzip -qq #{File.join(sensor_dir, 'SensorInstaller.zip')} -d #{@droplet.sandbox} 2>&1"
+        shell "unzip -qq #{File.join(sensor_dir, 'SeekerInstaller.jar')} #{AGENT_JARS_RELATIVE_PATH_IN_JAR} \\
+          -d #{sensor_dir} 2>&1"
         shell "rm -rf #{sensor_dir}"
         @droplet.copy_resources
       end
@@ -52,7 +54,8 @@ module JavaBuildpack
       # verify required agent configuration is present
       def assert_configuration_valid(credentials)
         mandatory_config_keys =
-          [ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY, SEEKER_HOST_SERVICE_CONFIG_KEY, SEEKER_HOST_PORT_SERVICE_CONFIG_KEY]
+          [ENTERPRISE_SERVER_URI_SERVICE_CONFIG_KEY, SEEKER_HOST_SERVICE_CONFIG_KEY,
+           SEEKER_HOST_PORT_SERVICE_CONFIG_KEY]
         mandatory_config_keys.each do |config_key|
           raise "'#{config_key}' credential must be set" unless credentials[config_key]
         end
@@ -80,7 +83,7 @@ module JavaBuildpack
       SENSOR_ZIP_RELATIVE_PATH_AT_ENTERPRISE_SERVER = 'rest/ui/installers/binaries/LINUX'
 
       # Relative path of the agent jars after Sensor extraction
-      AGENT_JARS_RELATIVE_PATH_IN_JAR ='inline/agents/java/'
+      AGENT_JARS_RELATIVE_PATH_IN_JAR = 'inline/agents/java/'
 
       # seeker service name identifier
       FILTER = /seeker/
