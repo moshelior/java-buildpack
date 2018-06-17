60d557bd7f1d6800889db39486b2a8ad404cbca8
Moshe Apelbaum <apelbaum@synopsys.com>
2018-06-17 16:20:35 +0300

    use new server API for agent download + tests

--- lib/java_buildpack/framework/seeker_security_provider.rb
+++ lib/java_buildpack/framework/seeker_security_provider.rb
@@ -25,27 +25,29 @@ module JavaBuildpack
     class SeekerSecurityProvider < JavaBuildpack::Component::BaseComponent
       # (see JavaBuildpack::Component::BaseComponent#detect)
       def detect
-        true
+        @application.services.one_service? FILTER
       end
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
         credentials = fetch_credentials
         assert_configuration_valid(credentials)
-        download_tar('', credentials[AGENT_ARTIFACT_SERVICE_CONFIG_KEY], false, @droplet.sandbox)
+        java_agent_zip_uri = agent_direct_link(credentials)
+        puts "Before downloading Sensor from: #{java_agent_zip_uri}"
+        download_zip('', java_agent_zip_uri, false, @droplet.sandbox)
         @droplet.copy_resources
       end
 
       # extract seeker relevant configuration as map
       def fetch_credentials
-        service = @application.services.find_service FILTER, SEEKER_HOST_SERVICE_CONFIG_KEY
+        service = @application.services.find_service FILTER, SEEKER_HOST_CONFIG_KEY
         service['credentials']
       end
 
       # verify required agent configuration is present
       def assert_configuration_valid(credentials)
         mandatory_config_keys =
-          [AGENT_ARTIFACT_SERVICE_CONFIG_KEY, SEEKER_HOST_SERVICE_CONFIG_KEY, SEEKER_HOST_PORT_SERVICE_CONFIG_KEY]
+          [ENTERPRISE_SERVER_URI_CONFIG_KEY, SEEKER_HOST_CONFIG_KEY, SEEKER_HOST_PORT_CONFIG_KEY]
         mandatory_config_keys.each do |config_key|
           raise "'#{config_key}' credential must be set" unless credentials[config_key]
         end
@@ -56,23 +58,31 @@ module JavaBuildpack
         credentials = fetch_credentials
         @droplet.java_opts.add_javaagent(@droplet.sandbox + 'seeker-agent.jar')
         @droplet.environment_variables
-                .add_environment_variable('SEEKER_SENSOR_HOST', credentials[SEEKER_HOST_SERVICE_CONFIG_KEY])
-                .add_environment_variable('SEEKER_SENSOR_HTTP_PORT', credentials[SEEKER_HOST_PORT_SERVICE_CONFIG_KEY])
+                .add_environment_variable('SEEKER_SENSOR_HOST', credentials[SEEKER_HOST_CONFIG_KEY])
+                .add_environment_variable('SEEKER_SENSOR_HTTP_PORT', credentials[SEEKER_HOST_PORT_CONFIG_KEY])
       end
 
       # JSON key for the host of the seeker sensor
-      SEEKER_HOST_SERVICE_CONFIG_KEY = 'sensor_host'
+      SEEKER_HOST_CONFIG_KEY = 'sensor_host'
 
       # JSON key for the port of the seeker sensor
-      SEEKER_HOST_PORT_SERVICE_CONFIG_KEY = 'sensor_port'
+      SEEKER_HOST_PORT_CONFIG_KEY = 'sensor_port'
 
-      # In the future Seeker's will expose REST endpoint for downloading the agent from the enterprise server (tgz file)
-      AGENT_ARTIFACT_SERVICE_CONFIG_KEY = 'agent_uri'
+      # JSON key for the enterprise server URL
+      ENTERPRISE_SERVER_URI_CONFIG_KEY = 'enterprise_server_url'
+
+      AGENT_PATH = '/rest/ui/installers/agents/binaries/JAVA'
+
       # seeker service name identifier
       FILTER = /seeker/
 
-      private_constant :SEEKER_HOST_SERVICE_CONFIG_KEY, :SEEKER_HOST_PORT_SERVICE_CONFIG_KEY,
-                       :AGENT_ARTIFACT_SERVICE_CONFIG_KEY, :AGENT_ARTIFACT_SERVICE_CONFIG_KEY
+      private_constant :SEEKER_HOST_CONFIG_KEY, :SEEKER_HOST_PORT_CONFIG_KEY,
+                       :ENTERPRISE_SERVER_URI_CONFIG_KEY, :AGENT_PATH
+      private
+
+      def agent_direct_link(credentials)
+        URI.join(credentials[ENTERPRISE_SERVER_URI_CONFIG_KEY], AGENT_PATH).to_s
+      end
     end
   end
 end
