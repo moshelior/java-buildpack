8b7a7c2bf4aa366a0013f04bbe39f14aaddc3759
Moshe Apelbaum <apelbaum@synopsys.com>
2018-07-15 16:14:43 +0300

    logging

--- lib/java_buildpack/framework/seeker_security_provider.rb
+++ lib/java_buildpack/framework/seeker_security_provider.rb
@@ -45,7 +45,7 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::BaseComponent#compile)
 
       def compile
-        @logger.debug { 'Seeker buildpack compile stage start 10' }
+        @logger.debug { 'Seeker buildpack compile stage start' }
         credentials = fetch_credentials
         @logger.debug { "Credentials #{credentials}" }
         assert_configuration_valid(credentials)
@@ -75,8 +75,8 @@ module JavaBuildpack
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
-        @logger.debug { 'Seeker buildpack release stage start' }
         credentials = fetch_credentials
+        @logger.debug { 'Seeker buildpack release stage start' }
         @droplet.java_opts.add_javaagent(@droplet.sandbox + 'seeker-agent.jar')
         @droplet.environment_variables
           .add_environment_variable('SEEKER_SENSOR_HOST', credentials[SENSOR_HOST_SERVICE_CONFIG_KEY])
